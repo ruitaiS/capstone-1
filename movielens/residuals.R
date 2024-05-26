7a3aab0d5064a15607acd9cd9ba7bef305444869
Peter Shao <shaoruitai@gmail.com>
2024-05-25 20:15:37 -0400

    .

--- movielens/residuals.R
+++ movielens/residuals.R
@@ -15,70 +15,18 @@ train_df$userIndex <- as.numeric(factor(train_df$userId))
 train_df$movieIndex <- as.numeric(factor(train_df$movieId))
 
 # Test Only: Check that the indices are all consecutive, and all match
-all(diff(sort(users$userIndex)) == 1)
-all(diff(sort(movies$movieIndex)) == 1)
-all(users$userIndex[match(train_df$userId, users$userId)] == train_df$userIndex)
-all(movies$movieIndex[match(train_df$movieId, movies$movieId)] == train_df$movieIndex)
+#all(diff(sort(users$userIndex)) == 1)
+#all(diff(sort(movies$movieIndex)) == 1)
+#all(diff(sort(unique(train_df$userIndex))) == 1)
+#all(diff(sort(unique(train_df$movieIndex))) == 1)
+#all(users$userIndex[match(train_df$userId, users$userId)] == train_df$userIndex)
+#all(movies$movieIndex[match(train_df$movieId, movies$movieId)] == train_df$movieIndex)
+
+# Test Only: Look up indices from ids in test_df
+#users$userIndex[match(test_df$userId, users$userId)]
+#movies$movieIndex[match(test_df$movieId, movies$movieId)]
 
 
-
-
-# Plotting the Residuals Density Plot
-density_values <- density(train_df$r)
-plot(density_values, main = "Density Plot of Remaining Variation", xlab = "Remainder", ylab = "Density")
-polygon(density_values, col = "lightblue", border = "black")
-
-# Comparing Residuals of Two Movies
-compare_r <- function(movieId1, movieId2){
-  subset <- merge(train_df[train_df$movieId == movieId1, c('movieId', 'userId', 'title', 'r')],
-                         train_df[train_df$movieId == movieId2, c('movieId', 'userId', 'title', 'r')],
-                         by = "userId")
-  title1 <- unique(subset$title.x)[1]
-  title2 <- unique(subset$title.y)[1]
-  
-  ggplot(subset, aes(x = r.x, y = r.y)) +
-    geom_point() +
-    xlab(paste(title1, " Residuals")) +
-    ylab(paste(title2, " Residuals")) +
-    ggtitle(paste("Comparison of Residuals for", title1, "and", title2))
-}
-
-#compare_r(1, 300)
-
-# SVD on Residuals----------------
-
-# Exploration --------
-
-set.seed(1987)
-n <- 100
-k <- 8
-Sigma <- 64  * matrix(c(1, .75, .5, .75, 1, .5, .5, .5, 1), 3, 3) 
-m <- MASS::mvrnorm(n, rep(0, 3), Sigma)
-m <- m[order(rowMeans(m), decreasing = TRUE),]
-y <- m %x% matrix(rep(1, k), nrow = 1) + matrix(rnorm(matrix(n*k*3)), n, k*3)
-colnames(y) <- c(paste(rep("Math",k), 1:k, sep="_"),
-                 paste(rep("Science",k), 1:k, sep="_"),
-                 paste(rep("Arts",k), 1:k, sep="_"))
-
-my_image <- function(x, zlim = range(x), ...){
-  colors = rev(RColorBrewer::brewer.pal(9, "RdBu"))
-  cols <- 1:ncol(x)
-  rows <- 1:nrow(x)
-  image(cols, rows, t(x[rev(rows),,drop=FALSE]), xaxt = "n", yaxt = "n",
-        xlab="", ylab="",  col = colors, zlim = zlim, ...)
-  abline(h=rows + 0.5, v = cols + 0.5)
-  axis(side = 1, cols, colnames(x), las = 2)
-}
-
-my_image(y)
-
-# TODO
-
-my_image(cor(y), zlim = c(-1,1))
-range(cor(y))
-axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
-
-#---------
 
 fill_missing_A <- function(i, u){
   # TODO: Default Values for residuals on missing user/movie combinations
@@ -100,33 +48,30 @@ fill_missing_A <- function(i, u){
   # r = alpha - (movie_bias + user_bias + genre_bias)
   # You can just tune for alpha
   
-  return 0
+  return (0)
 }
-
-
 
 # Populate the residuals matrix
 residuals <- as.matrix(train_df[, c("userId", "movieId", "r")])
 
-# Exploration:
+# Fun Functions (Do Not Run)
 
-#A <- matrix(c(13, -4, 2, -4, 11, -2, 2, -2, 8), 3, 3, byrow=TRUE)
-#ev <- eigen(A)
-#eigenvalues <- ev$values
-#eigenvectors <- ev$vectors
+# Plotting the Residuals Density Plot
+density_values <- density(train_df$r)
+plot(density_values, main = "Density Plot of Remaining Variation", xlab = "Remainder", ylab = "Density")
+polygon(density_values, col = "lightblue", border = "black")
 
-
-# Exploration:
-
-#random_movieIds <- sample(unique(train_df$movieId), 100, replace = FALSE)
-
-# Randomly select 100 unique userIds
-#random_userIds <- sample(unique(train_df$userId), 100, replace = FALSE)
-
-# Filter train_df based on randomly selected movieIds and userIds
-#filtered_df <- train_df[train_df$movieId %in% random_movieIds & train_df$userId %in% random_userIds, ]
-
-# Convert filtered dataframe to matrix
-#residuals <- as.matrix(filtered_df[, c("userId", "movieId", "r")])
-
-
+# Comparing Residuals of Two Movies
+compare_r <- function(movieId1, movieId2){
+  subset <- merge(train_df[train_df$movieId == movieId1, c('movieId', 'userId', 'title', 'r')],
+                  train_df[train_df$movieId == movieId2, c('movieId', 'userId', 'title', 'r')],
+                  by = "userId")
+  title1 <- unique(subset$title.x)[1]
+  title2 <- unique(subset$title.y)[1]
+  
+  ggplot(subset, aes(x = r.x, y = r.y)) +
+    geom_point() +
+    xlab(paste(title1, " Residuals")) +
+    ylab(paste(title2, " Residuals")) +
+    ggtitle(paste("Comparison of Residuals for", title1, "and", title2))
+}
