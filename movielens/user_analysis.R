481bcf128ae4d6a3547189c39efdf660d2d92114
Peter Shao <shaoruitai@gmail.com>
2024-06-01 20:02:04 -0400

    I will literally never laugh at this

--- movielens/user_analysis.R
+++ movielens/user_analysis.R
@@ -12,25 +12,28 @@ plot <- ggplot(users, aes(x = count)) +
   theme_minimal()+
   theme(
     text = element_text(size = 20),          # General text size
-    plot.title = element_text(size = 35),    # Title text size
-    axis.title = element_text(size = 30),    # Axis titles text size
-    axis.text = element_text(size = 25)      # Axis text size
+    plot.title = element_text(size = 45),    # Title text size
+    axis.title = element_text(size = 40),    # Axis titles text size
+    axis.text = element_text(size = 35)      # Axis text size
   )
 
 store_plot("cum_density.png", h=1500, w=1500, plot)
 
 # Histogram
-ggplot(users, aes(x = count)) +
-  geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) +
-  labs(title = "Histogram of Count Values",
-       x = "Count",
-       y = "Frequency") +
-  theme_minimal()
+
 
 # Create a box-and-whisker plot for each decile
-ggplot(user_percentiles, aes(x = as.factor(decile), y = count)) +
+plot <- ggplot(user_percentiles, aes(x = as.factor(decile), y = count)) +
   geom_boxplot(fill = "blue", alpha = 0.7) +
-  labs(title = "Box-and-Whisker Plot by Decile",
+  labs(title = "Box-and-Whisker Plot of Rating Counts by Decile",
        x = "Decile",
        y = "Count") +
-  theme_minimal()
+  theme_minimal()+
+  theme(
+    text = element_text(size = 20),          # General text size
+    plot.title = element_text(size = 45),    # Title text size
+    axis.title = element_text(size = 40),    # Axis titles text size
+    axis.text = element_text(size = 35)      # Axis text size
+  )
+
+store_plot("box-whisker-decile.png", h=1500, w=1500, plot)
